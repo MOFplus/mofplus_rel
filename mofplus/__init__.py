ddae3d459bb380ac1111d57658b41a583b299c78
Rochus Schmid <rochus.schmid@rub.de>
2017-03-27 12:18:14 +0200

    added afdict to aftype.py (and some bugfixes)

--- mofplus/__init__.py
+++ mofplus/__init__.py
@@ -1,5 +1,6 @@
 from user import user_api
 from ff import FF_api
 from admin import admin_api
+from aftype import aftype, aftype_sort, afdict
 
-__all__ = ["user_api", "FF_api", "admin_api"]
+__all__ = ["user_api", "FF_api", "admin_api", "aftype", "aftype_sort", "afdict"]
